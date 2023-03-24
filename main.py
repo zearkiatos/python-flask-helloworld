113dd79936fdc89a2f56688ad29330c82b141488
zearkiatos <caprilespe@outlook.com>
2023-03-23 21:33:20 -0300

    Config error pages

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, abort
 
 app = Flask(__name__)
 
 todos = ['Practice Python', 'Practice Javascript', 'Practice ReactJS']
+
+NOT_FOUND = 404
+INTERNAL_SERVER_ERROR=500
+
+
+@app.errorhandler(NOT_FOUND)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
 
 
 @app.route("/")
@@ -16,23 +28,37 @@ def index():
 
 @app.route("/hello")
 def hello():
-    return "Hello World! 👋 🌎 Flask 🌶️"
+    try:
+        return "Hello World! 👋 🌎 Flask 🌶️"
+    except Exception:
+        abort(INTERNAL_SERVER_ERROR)
 
 
 @app.route("/user-ip")
 def user_ip():
-    user_ip = request.cookies.get("user_ip")
-    return "Your IP es {}".format(user_ip)
+    try:
+        user_ip = request.cookies.get("user_ip")
+        return "Your IP es {}".format(user_ip)
+    except Exception:
+        abort(INTERNAL_SERVER_ERROR)
 
 
 @app.route("/user-ip-page")
 def user_ip_page():
-    user_ip = request.cookies.get("user_ip")
-    context = {
-        'user_ip': user_ip,
-        'todos': todos
-    }
-    return render_template("hello.html", **context)
+    try:
+        user_ip = request.cookies.get("user_ip")
+        context = {
+            'user_ip': user_ip,
+            'todos': todos
+        }
+        return render_template("hello.html", **context)
+    except Exception:
+        abort(INTERNAL_SERVER_ERROR)
+
+@app.route("/force-internal-server-error")
+def force_internal_server_error():
+    abort(INTERNAL_SERVER_ERROR)
+
 
 
 if __name__ == "__main__":
